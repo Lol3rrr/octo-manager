2136f7a93afd49e7ef0be32ea377308f6c4d5509
lol3rrr <s.loler03@gmail.com>
2020-04-06 23:02:06 +0200

    Fixed remote write

--- backup/remote/writeToFile.go
+++ backup/remote/writeToFile.go
@@ -11,7 +11,7 @@ import (
 func writeToFile(content, path string, sshClient *ssh.Client) error {
 	writeCmd := fmt.Sprintf("cat > %s \n", path)
 
-	readContent := writeCmd + content + "^C"
+	readContent := writeCmd + content
 	cReader := strings.NewReader(readContent)
 	cWriter := bytes.NewBuffer([]byte{})
 	shell := sshClient.Shell().SetStdio(cReader, cWriter, cWriter)
