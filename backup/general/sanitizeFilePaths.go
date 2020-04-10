f04afbd2d63ec195906d871f4cbbb60622c9f1d3
lol3rrr <s.loler03@gmail.com>
2020-04-10 17:19:46 +0200

    Changed the way the paths work a bit

--- backup/general/sanitizeFilePaths.go
+++ backup/general/sanitizeFilePaths.go
@@ -4,5 +4,5 @@ import "strings"
 
 // SanitizePath removes the given ServerDir string from the path
 func (file *File) SanitizePath(serverDir string) {
-	file.Path = strings.ReplaceAll(file.Path, serverDir, "")
+	file.Path = strings.ReplaceAll(file.Path, serverDir+"/", "")
 }
