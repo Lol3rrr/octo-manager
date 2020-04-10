cbf6ffbf7af06cc4a5fd4f55f1f6f9f65024a340
lol3rrr <s.loler03@gmail.com>
2020-04-10 15:47:51 +0200

    Added a small comment for inline docs

--- backup/local/getBackupDirs.go
+++ backup/local/getBackupDirs.go
@@ -5,6 +5,8 @@ import (
 	"octo-manager/backup/general"
 )
 
+// getBackupDirs returns all the directorys that correspond to
+// backups in the given path
 func getBackupDirs(path string) ([]dirInfo, error) {
 	fileInfos, err := ioutil.ReadDir(path)
 	if err != nil {
